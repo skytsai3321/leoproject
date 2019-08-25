084154e1eaf6ac2558fe1cc4790a7c84c95cdab9
Leo Tsai <skytsai3321@gmail.com>
2019-08-25 17:46:18 +0800

    Initial commit

---
+++ src/main/kotlin/com/hank/Hello.kt
@@ -0,0 +1,13 @@
+package com.hank
+
+fun main() {
+//    println("Hello Kotlin")
+    val h = Human()
+    h.hello()
+}
+
+class Human() {
+    fun hello() {
+        println("Hello kotlin")
+    }
+}
